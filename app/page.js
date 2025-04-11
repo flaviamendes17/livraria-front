d893f8800800d74ed5c7b727c397b86c95f13fca
Julianeves01 <julianvs2007@gmail.com>
2025-04-10 21:32:42 -0300

    finished

--- app/page.js
+++ app/page.js
@@ -2,10 +2,9 @@ import Header from "../components/Header";
 import Banner from "../components/Banner.jsx";
 import CategoriaList from "../components/CategoriaList";
 import Footer from "../components/Footer";
-import Lancamentos from "../components/Lancamentos";
-import LancamentosDestaque from "@/components/LancamentosDestaque";
+import LancamentosDestaque from "../components/LancamentosDestaque";
 import Newletter from "../components/Newletter";
-import Destaques from "../components/Destaques"; // Corrigido: Importação do componente correto
+import DestaquesLivros from "../components/DestaquesLivros";
 
 const categorias = [
     { name: "Ficção Científica", image: "/ficcao-cientifica.png" },
@@ -22,11 +21,10 @@ export default function Page() {
     return (
         <div>
             <Header />
-            <Destaques categorias={categorias}> {/* Passando a lista de categorias */}
-            </Destaques>
+            <DestaquesLivros />
             <LancamentosDestaque />
             <Newletter />
-            <Footer /> 
+            <Footer />
         </div>
     );
 }
